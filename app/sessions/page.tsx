783f12c9aacc836a66b2598557b7f3863b9304f4
Jason Christopher <jason.christopher@u.nus.edu>
2025-06-17 23:44:44 +0800

    Integrate sessions list to database

--- app/sessions/page.tsx
+++ app/sessions/page.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Image from "next/image";
 import Header from "@/components/Header";
 import SessionBox from "./SessionBox";
+import { Session } from "@prisma/client";
 
 const dummyData = [
   {
@@ -44,14 +47,30 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar"
 import MemberGuard from "../common/authguard/MemberGuard";
+import { useSessionsQuery } from "@/queries/useSessionsQuery";
 
 export default function Page() {
+
+  const { data: sessions, isLoading } = useSessionsQuery();
+
+  if (isLoading) {
+    return <div className="flex justify-center items-center h-screen">Loading...</div>
+  }
+  if (!sessions || sessions.length === 0) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <span className="text-gray-500">No upcoming sessions available.</span>
+      </div>
+    );
+  }
+
+  console.log(sessions);
   return (
     <MemberGuard>
       <div className="px-8 py-8 flex flex-col gap-4 max-w-screen-xl mx-auto">
         <span className="font-heading font-bold text-[22px]">UPCOMING SESSIONS</span>
         <div className="flex flex-col gap-4 md:grid md:grid-cols-2 lg:grid-cols-3">
-          {dummyData.map((item) => <SessionBox props={item} key={item.id} />)}
+          {sessions.map((item: Session) => <SessionBox props={item} key={item.id} />)}
         </div>
       </div>
     </MemberGuard>
