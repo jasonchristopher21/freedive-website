94daa0de6d1b4f04ee1df4203d993528fd545ecd
yitao36 <yitao36g@gmail.com>
2025-12-11 13:04:36 +0800

    Style modifications

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { useAvatarQuery } from '@/queries/useAvatarQuery';
 import { useAppDispatch, useAppSelector } from "@/redux/store";
-import { CloseCircleOutlined, EditOutlined, PlusOutlined } from '@ant-design/icons';
+import { CheckOutlined, CloseCircleOutlined, EditOutlined, PlusOutlined } from '@ant-design/icons';
 import { User } from '@prisma/client';
 import { useEffect, useRef, useState } from "react";
 import MemberGuard from "../common/authguard/MemberGuard";
@@ -25,7 +25,7 @@ function SettingsPage() {
   return (
     <div className="flex flex-col px-8 py-8 min-w-full justify-center gap-4 max-w-screen-lg ml-0">
       <span className={styles.heading1}>SETTINGS</span>
-      <div className="p-4 border-2 border-grey-100 border-opacity-50 rounded-lg flex flex-col gap-2 w-fit
+      <div className="p-12 border-2 border-grey-100 border-opacity-50 rounded-lg flex flex-col gap-2 w-fit
                       md:px-20 md:py-12 md:gap-4">
 
         {/** Row 1 */}
@@ -36,33 +36,35 @@ function SettingsPage() {
 
         <div className="w-full border-t-2 border-grey-200 mt-4 mb-6" />
 
-        {/** Row 2 */}
-        <div className='flex flex-row gap-12 items-center'>
-          <h1 className={styles.heading1 + " min-w-[200]"}>Preferred name: </h1>
-          <h1 className={styles.heading1}>{user.preferredName}</h1>
-        </div>
+        <div className='flex flex-col pl-10 gap-4'>
 
-        <div className='flex flex-row gap-12 items-center'>
-          <h1 className={styles.heading1 + " min-w-[200]"}>Email: </h1>
-          <h1 className={styles.heading1}>{user.email}</h1>
-        </div>
-
-        <div className='flex flex-row gap-12 items-center'>
-          <h1 className={styles.heading1 + " min-w-[200]"}>NUSNet Email: </h1>
-          <h1 className={styles.heading1}>{user.nusnetEmail}</h1>
-        </div>
-
-        <div className='flex flex-row gap-12 items-center'>
-          <h1 className={styles.heading1 + " min-w-[200]"}>Year of Study: </h1>
-          <h1 className={styles.heading1}>{user.yearOfStudy}</h1>
-        </div>
-
-        {user.telegramHandle &&
+          {/** Row 2 */}
           <div className='flex flex-row gap-12 items-center'>
-            <h1 className={styles.heading1 + " min-w-[200]"}>Telegram Handle: </h1>
-            <h1 className={styles.heading1}>{user.telegramHandle}</h1>
+            <h1 className={styles.heading2 + " min-w-[200]"}>Preferred name: </h1>
+            <h1 className={styles.heading2}>{user.preferredName}</h1>
           </div>
-        }
+
+          <div className='flex flex-row gap-12 items-center'>
+            <h1 className={styles.heading2 + " min-w-[200]"}>Email: </h1>
+            <h1 className={styles.heading2}>{user.email}</h1>
+          </div>
+
+          <div className='flex flex-row gap-12 items-center'>
+            <h1 className={styles.heading2 + " min-w-[200]"}>NUSNet Email: </h1>
+            <h1 className={styles.heading2}>{user.nusnetEmail}</h1>
+          </div>
+
+          <div className='flex flex-row gap-12 items-center'>
+            <h1 className={styles.heading2 + " min-w-[200]"}>Year of Study: </h1>
+            <h1 className={styles.heading2}>{user.yearOfStudy}</h1>
+          </div>
+
+          <div className='flex flex-row gap-12 items-center'>
+            <h1 className={styles.heading2 + " min-w-[200]"}>Telegram Handle: </h1>
+            <h1 className={styles.heading2}>{user.telegramHandle}</h1>
+          </div>
+        </div>
+
       </div>
     </div>
   )
@@ -154,23 +156,23 @@ function EdittableAvatar({ user }: { user: User }) {
 }
 
 function EdittableText({ user }: { user: User }) {
+  const actionIconStyle = 'transition-all p-3 hover:bg-gray-200 rounded-full cursor-pointer'
+
   const [edit, setEdit] = useState<boolean>(false)
   const [editName, setEditName] = useState<string>(user.name)
 
   return (
-    <div className='flex min-w-[333] justify-between p-4 gap-4 items-center border-2 shadow-md'>
+    <div className='flex min-w-[300] p-4 items-center border-2 shadow-md'>
       {edit ?
         <>
-          <Input value={editName} onChange={e => setEditName(e.target.value)} className={styles.heading2} variant='underlined' />
-          <CloseCircleOutlined className='transition-all p-3 hover:bg-gray-200 rounded-full cursor-pointer' onClick={() => {
-            setEdit(false)
-            setEditName(user.name)
-          }} />
+          <Input value={editName} onChange={e => setEditName(e.target.value)} className={styles.heading2 + " mr-auto w-44"} variant='underlined' />
+          <CheckOutlined className={actionIconStyle} onClick={() => { }} />
+          <CloseCircleOutlined className={actionIconStyle} onClick={() => { setEdit(false); setEditName(user.name) }} />
         </>
         :
         <>
-          <h1 className={styles.heading1 + " pl-2"}>{user.name}</h1>
-          <EditOutlined className='transition-all p-3 hover:bg-gray-200 rounded-full cursor-pointer'
+          <h1 className={styles.heading1 + " mr-auto pl-2"}>{user.name}</h1>
+          <EditOutlined className={actionIconStyle}
             onClick={() => setEdit(true)} />
         </>
       }
