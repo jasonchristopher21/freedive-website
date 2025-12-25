4a6544140294cd16a913534d0d2f6acfe431894c
yitao36 <yitao36g@gmail.com>
2025-12-26 00:09:05 +0800

    Fix attempt 26

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: {
-      resolveAlias: {
-        ".prisma/client/index-browser": "./node_modules/.prisma/client/index-browser.js",
-      },
-    },
-  },
 }
 
 export default nextConfig
